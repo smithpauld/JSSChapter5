6f21c52b9bebee02e606e5c9c7e64bcf0411d5d0
smithpauld <u0128625@reallegal.local>
2017-02-13 11:09:06 -0700

    hilo branch

--- src/Program4.java
+++ src/Program4.java
@@ -5,21 +5,16 @@ public class Program4 {
 
 	public static void main(String[] args) {
 
-		int randomNumber, guessNumber;
+		int randomNumber, guessNumber, count = 0;
 		Random generator = new Random();
 		Scanner scan = new Scanner(System.in);
-		String quit ="n", another = "y";
+		String another = "y";
 
 		System.out.println("This is the Hi-Lo guessing game.");
 		System.out.println("The computer will pick a random number between 1 and 100");
 		System.out.println("After each guess you make, the computer will tell you " + "");
 		System.out.println("if your guess is to high or low");
-		
 
-		while (quit != "q")
-		{
-		
-		
 		while (another.equalsIgnoreCase("y"))
 
 		{
@@ -29,28 +24,28 @@ public class Program4 {
 			System.out.println("Please enter a number to play");
 			guessNumber = scan.nextInt();
 
-			while (guessNumber != randomNumber && quit =="n")
-			{
-				if (guessNumber > randomNumber)
+			while (guessNumber != randomNumber) {
+				if (guessNumber > randomNumber) {
 					System.out.println("The number you entered is too high!");
-				else if (guessNumber < randomNumber)
+					count++;
+				}  if (guessNumber < randomNumber) {
 					System.out.println("The number you entered is too low!");
-				else
+					count++;
+				} else {
 					System.out.println("The number you entered is the correct guess!");
-			
-			System.out.println("Please enter another guess");
-			System.out.println("If you would like to quit, select q");
-			guessNumber = scan.nextInt();
-			quit = scan.nextLine();
-			
+					count++;
+					
+				}
+
+				System.out.println("Please enter another guess");
+
+				guessNumber = scan.nextInt();
+
 			}
-			
-			
+			System.out.println("It took you " + count + " guesses!");
 			System.out.println("Do you want to play again? Selct Y or y");
 			another = scan.nextLine();
 
-		}
-		
 		}
 
 	}
